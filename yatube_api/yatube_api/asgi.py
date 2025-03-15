95e34f3f0296e0e4992c49da480d2a82426e2467
BlackGotz <83086847+BlackGotz@users.noreply.github.com>
2025-03-15 18:06:02 +0700

    Update

--- yatube_api/yatube_api/asgi.py
+++ yatube_api/yatube_api/asgi.py
@@ -1,9 +1,11 @@
 """
-ASGI config for yatube_api project.
+ASGI config for api_final_yatube project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+Этот файл содержит настройки для запуска проекта на ASGI-сервере.
+ASGI (Asynchronous Server Gateway Interface) обеспечивает поддержку асинхронных веб-приложений,
+что позволяет обрабатывать запросы более эффективно и поддерживать такие протоколы, как WebSocket.
 
-For more information on this file, see
+Для получения дополнительной информации об ASGI и его использовании в Django, посетите:
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
@@ -11,6 +13,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+
+# Устанавливаем переменную окружения для настроек Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube_api.settings')
 
+# Получаем ASGI-приложение для использования сервером
 application = get_asgi_application()
