0205dc625ad7c6d64c8f6bcc7a2253b7f044f7b7
BlackGotz <83086847+BlackGotz@users.noreply.github.com>
2025-03-15 18:06:49 +0700

    Update

--- yatube_api/yatube_api/wsgi.py
+++ yatube_api/yatube_api/wsgi.py
@@ -1,9 +1,11 @@
 """
-WSGI config for yatube_api project.
+WSGI config for api_final_yatube project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+Этот файл содержит настройки для запуска проекта на WSGI-сервере.
+WSGI (Web Server Gateway Interface) является стандартом для синхронных веб-приложений на Python
+и широко используется для развертывания Django-приложений в продакшене.
 
-For more information on this file, see
+Для получения дополнительной информации о WSGI и его использовании в Django, посетите:
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
@@ -11,6 +13,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Устанавливаем переменную окружения для настроек Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube_api.settings')
 
+# Получаем WSGI-приложение для использования сервером
 application = get_wsgi_application()
